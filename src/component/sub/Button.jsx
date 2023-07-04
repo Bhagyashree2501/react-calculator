f166a913a2a6cb2ae34afb0554b4a309f6bb8dcd
Bhagyashree2501 <bhagyashreewankhede25@gmail.com>
2023-07-04 16:44:20 +0530

    react calculator

---
+++ src/component/sub/Button.jsx
@@ -0,0 +1,9 @@
+import Ract from 'react'
+
+function Button(props) {
+    return (
+        <button className={props.class} onClick={() => props.handler(props.title)} > { props.title }</button>
+    )
+}
+
+export default Button
